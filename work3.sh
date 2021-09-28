a94216a5b2888dd639aaed860c88ab8cf046541a
rakdman <59464659+rakdman@users.noreply.github.com>
2021-09-28 05:37:27 +0200

    Add files via upload

---
+++ work3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SERVER_PATH=/home/ubuntu/DebtNotificationSystem/server
+CLIENT_PATH=/home/ubuntu/DebtNotificationSystem/server
+git clone https://github.com/rakdman/DebtNotificationSystem.git
+sudo apt-get update
+sudo apt-get install -y nodejs
+sudo apt-get update
+sudo apt-get install npm
+cd $SERVER_PATH
+npm install
+npm start &
+sleep 10
+cd $CLIENT_PATH
+npm install
+npm start &
+sleep 10
